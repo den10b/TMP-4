4b72e4fd77d4e387164b3737ac22551552d7dcbf
den10b <den10b@yandex.ru>
2020-10-07 11:51:09 +0300

    3

--- prakt43/prakt43/Program.cs
+++ prakt43/prakt43/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
-
-namespace prakt43
+﻿using System.Collections;
+using System;
+class Program
 {
-  class Program
+  static void Main(string[] args)
   {
-    static void Main(string[] args)
+    Hashtable lookup = new Hashtable();
+
+    lookup["0"] = "Zero";
+    lookup["1"] = "One";
+    lookup["2"] = "Two";
+    lookup["3"] = "Three";
+    lookup["4"] = "Four";
+    lookup["5"] = "Five";
+    lookup["6"] = "Six";
+    lookup["7"] = "Seven";
+    lookup["8"] = "Eight";
+    lookup["9"] = "Nine";
+
+
+
+    string ourNumber = "888 - 555 - 1212";
+  foreach (char с in ourNumber)
     {
-      Console.WriteLine("Hello World!");
+      string digit = с.ToString(); if (lookup.ContainsKey(digit))
+      {
+        Console.WriteLine(lookup[digit]);
+      }
     }
   }
 }
